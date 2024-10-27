9c8a5b221e967a4ca3fce634b66f23ec7b9fcf70
MadhusriChamakuri0518 <madhusrichamakuri18@gmail.com>
2024-10-27 22:09:15 +0530

    day-2 assignment

---
+++ src/components/atoms/AtomClass.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class AtomClass extends Component {
+    render() {
+        return <h1>Hello from Atom Class Components</h1>
+    }
+}
+
+export {AtomClass}
---
+++ src/components/atoms/Atomfunc.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Atomfunc(){
+    return <h1>Hello atoms from Functional component</h1>
+}
+
+export default Atomfunc
